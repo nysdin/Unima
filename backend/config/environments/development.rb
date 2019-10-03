bac4ca67816e8c92a7e273f6bb297d628a60c321
ShiniYanada <ysthon@gmail.com>
2019-10-03 13:09:51 +0900

    smtp_settingsの:addressをホストのipアドレスに変更

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '192.168.144.1', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => '172.26.0.1', :port => 1025 }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
 
